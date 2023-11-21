440b1ff2892e03ffcc6545246c65e828a336437a
Cr9Co0 <alburton32@gmail.com>
2023-11-21 10:17:45 +0500

    Add a full_tittle helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  #Return the full title on per-page basis
+  def full_title(page_title = '')
+    base_title = "Cr9Co0"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
